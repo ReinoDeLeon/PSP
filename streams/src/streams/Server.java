6df1782c1d1f7f7dbf6f17456027629ece3c3bb5
Iván <ivanantonsagastizabal@gmail.com>
2022-01-25 10:53:34 +0100

    Added comments to clarify the code

--- streams/src/streams/Server.java
+++ streams/src/streams/Server.java
@@ -1,6 +1,10 @@
 package streams;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,7 +12,7 @@ public class Server {
 	private static final int MAX_PORT_NUMBER = 65535;
 	private static final int MIN_PORT_NUMBER = 1;
 	public static void main(String[] args) throws IOException {
-		if (args.length!=1) {
+		if (args.length!=1) { // No args
 			System.err.println("Usage: java Sever <port number>");
 			System.exit(1);
 		}
@@ -16,18 +20,29 @@ public class Server {
 		try {
 			portNumber = Integer.parseInt(args[0]);
 		}
-		catch (NumberFormatException e) {
+		catch (NumberFormatException e) { // Args not a number
 			System.err.println("Error: java Sever <port number> must be a number");
 			System.exit(1);
 		}
-		if (portNumber< MIN_PORT_NUMBER || portNumber > MAX_PORT_NUMBER) {
+		if (portNumber< MIN_PORT_NUMBER || portNumber > MAX_PORT_NUMBER) { // Port number invalid
 			System.err.printf("Error: java Sever <port number> value must be an integer between %d and %d\n", MIN_PORT_NUMBER, MAX_PORT_NUMBER);
 			System.exit(1);
 		}
 		try (	
 				ServerSocket serverSocket = new ServerSocket(portNumber);
 				Socket clientSocket = serverSocket.accept();
+				BufferedReader socketIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				PrintWriter socketOut = new PrintWriter(clientSocket.getOutputStream(), true); // True so it sends the data automatically 				
 				) {
+			String line;
+				while ((line = socketIn.readLine()) != null) { // We read from client, null --> client closed conection
+					socketOut.println(line); 
+				}
+				
 			}
+		catch (BindException e) { // Port in use
+			System.err.printf("Error: port %d is already in use\n", portNumber);
+			System.exit(1);
+		}
 	}
 }
