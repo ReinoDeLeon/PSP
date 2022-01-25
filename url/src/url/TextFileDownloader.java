d7c97961f8da0672ca751c1ff1f279c8025e5cdb
Iván <ivanantonsagastizabal@gmail.com>
2022-01-25 09:16:11 +0100

    Small error on import

--- url/src/url/TextFileDownloader.java
+++ url/src/url/TextFileDownloader.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
