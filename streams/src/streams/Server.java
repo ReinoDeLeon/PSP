6fc753b988f9f0cccb7fc60ac0ada9cc690a7ea8
Iván <ivanantonsagastizabal@gmail.com>
2022-01-25 10:27:14 +0100

    Added checks for correct program working

--- streams/src/streams/Server.java
+++ streams/src/streams/Server.java
@@ -5,12 +5,29 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-	
+	private static final int MAX_PORT_NUMBER = 65535;
+	private static final int MIN_PORT_NUMBER = 1;
 	public static void main(String[] args) throws IOException {
-		try (	
-			ServerSocket serverSocket = new ServerSocket(1024);
-			Socket clientSocket = serverSocket.accept()) {
-				
+		if (args.length!=1) {
+			System.err.println("Usage: java Sever <port number>");
+			System.exit(1);
 		}
+		int portNumber = 0;
+		try {
+			portNumber = Integer.parseInt(args[0]);
+		}
+		catch (NumberFormatException e) {
+			System.err.println("Error: java Sever <port number> must be a number");
+			System.exit(1);
+		}
+		if (portNumber< MIN_PORT_NUMBER || portNumber > MAX_PORT_NUMBER) {
+			System.err.printf("Error: java Sever <port number> value must be an integer between %d and %d\n", MIN_PORT_NUMBER, MAX_PORT_NUMBER);
+			System.exit(1);
+		}
+		try (	
+				ServerSocket serverSocket = new ServerSocket(portNumber);
+				Socket clientSocket = serverSocket.accept();
+				) {
+			}
 	}
 }
